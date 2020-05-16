4e33f433fd54ad7693d15b06ba91b3bcbb5f5fe3
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-05-16 09:34:23 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProconTEL.Sdk
+[Link to documentation](#https://github.com/Macrix/procontel/blob/master/README.md)
---
+++ samples/PctML/FSharpEndpoint.fs
@@ -0,0 +1,14 @@
+﻿namespace PctML
+open ProconTel.Sdk.Attributes
+open ProconTel.Sdk.Services
+open System.Threading.Tasks
+
+  [<EndpointMetadata(Name = "FSharp", UseCustomSubscriberConfiguration = true, SupportedRoles = SupportedRoles.Both)>]
+  type FSharpEndpoint(logger: ILogger) = 
+    interface ProconTel.Sdk.Builders.IEndpointLifeTimeCycle with
+      member this.InitializeAsync() = 
+        logger.Information "F# start"
+        Task.CompletedTask
+      member this.TerminateAsync() = 
+        logger.Information "F# done"
+        Task.CompletedTask
