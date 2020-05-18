b44e98a42d7b825ef86da3554d0cb26fc756fa3f
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-05-18 08:32:06 +0200

    fix init code

--- samples/PctML/FSharpEndpoint.fs
+++ samples/PctML/FSharpEndpoint.fs
@@ -2,12 +2,13 @@
 open ProconTel.Sdk.Attributes
 open ProconTel.Sdk.Services
 open System.Threading.Tasks
+open System.Reflection
 
   [<EndpointMetadata(Name = "FSharp", UseCustomSubscriberConfiguration = true, SupportedRoles = SupportedRoles.Both)>]
   type FSharpEndpoint(logger: ILogger) = 
     interface ProconTel.Sdk.Builders.IEndpointLifeTimeCycle with
       member this.InitializeAsync() = 
-        logger.Information "F# start"
+        logger.Information("F# start, version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString())
         Task.CompletedTask
       member this.TerminateAsync() = 
         logger.Information "F# done"
