b43656e70a1e1b1204385ed12d8ecb116d0baf90
Jaroslaw Kurdzieko <jaroslaw.kurdzieko@macrix.pl>
2020-08-20 10:57:45 +0200

    release version

--- samples/PctML/FSharpEndpoint.fs
+++ samples/PctML/FSharpEndpoint.fs
@@ -9,7 +9,7 @@ open System.Reflection
     interface ProconTel.Sdk.Builders.IEndpointLifeTimeCycle with
       member this.InitializeAsync() = 
         logger.Information("F# start, version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString())
-        logger.Information("F# new version for presentation purpose 2")
+        logger.Information("Demo")
         Task.CompletedTask
       member this.TerminateAsync() = 
         logger.Information "F# done"
